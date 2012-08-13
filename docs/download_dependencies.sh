1f642cf7a471d8f385f0986bf8455f1b7d8731e3
slcornford <slcornford@4b1697f3-9003-4f1e-b6a8-e72787e6a227>
2012-08-13 13:29:13 +0000

    added some python interface docs
    git-svn-id: https://anag-repo.lbl.gov/svn/BISICLES/public/trunk@1617 4b1697f3-9003-4f1e-b6a8-e72787e6a227

--- docs/download_dependencies.sh
+++ docs/download_dependencies.sh
@@ -1,12 +1,15 @@
 cd $BISICLES_HOME
+echo `pwd`
 
 #get hdf5 sources
 if !(test -e hdf5-1.6.10.tar.gz) then
     echo "downloading hdf5-1.6.10.tar.gz"
     wget http://www.hdfgroup.org/ftp/HDF5/prev-releases/hdf5-1.6.10/src/hdf5-1.6.10.tar.gz
 fi
-
+mkdir -p hdf5/parallel/src
 tar -zxf hdf5-1.6.10.tar.gz -C hdf5/parallel/src
+
+mkdir -p hdf5/serial/src
 tar -zxf hdf5-1.6.10.tar.gz -C hdf5/serial/src
 
 #get netcdf sources
@@ -15,8 +18,10 @@ if !(test -e netcdf-4.1.2.tar.gz) then
     echo "downloading netcdf-4.1.2.tar.gz"
     wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.1.2.tar.gz
 fi
-
+mkdir -p netcdf/parallel/src
 tar -zxf netcdf-4.1.2.tar.gz -C netcdf/parallel/src
+
+mkdir -p netcdf/serial/src
 tar -zxf netcdf-4.1.2.tar.gz -C netcdf/serial/src
 
 
