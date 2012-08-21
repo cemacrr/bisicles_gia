4afade30b39e969c7696aa615a2f5a6dbae6fc29
slcornford <slcornford@4b1697f3-9003-4f1e-b6a8-e72787e6a227>
2012-08-21 12:43:14 +0000

    revert to hdf5 1.6.9 bacause 1.8.9 seems to violate C++ standard (trailing commas in enum)
    git-svn-id: https://anag-repo.lbl.gov/svn/BISICLES/public/trunk@1646 4b1697f3-9003-4f1e-b6a8-e72787e6a227

--- docs/download_dependencies.sh
+++ docs/download_dependencies.sh
@@ -2,27 +2,27 @@ cd $BISICLES_HOME_PARENT
 echo `pwd`
 
 #get hdf5 sources
-#if !(test -e hdf5-1.6.10.tar.gz) then
-#    echo "downloading hdf5-1.6.10.tar.gz"
-#    wget http://www.hdfgroup.org/ftp/HDF5/prev-releases/hdf5-1.6.10/src/hdf5-1.6.10.tar.gz
-#fi
-#mkdir -p hdf5/parallel/src
-#tar -zxf hdf5-1.6.10.tar.gz -C hdf5/parallel/src
+if !(test -e hdf5-1.6.10.tar.gz) then
+    echo "downloading hdf5-1.6.10.tar.gz"
+    wget http://www.hdfgroup.org/ftp/HDF5/prev-releases/hdf5-1.6.10/src/hdf5-1.6.10.tar.gz
+fi
+mkdir -p hdf5/parallel/src
+tar -zxf hdf5-1.6.10.tar.gz -C hdf5/parallel/src
 
-#mkdir -p hdf5/serial/src
-#tar -zxf hdf5-1.6.10.tar.gz -C hdf5/serial/src
+mkdir -p hdf5/serial/src
+tar -zxf hdf5-1.6.10.tar.gz -C hdf5/serial/src
 
 
 #get hdf5 sources
-if !(test -e hdf5-1.8.9.tar.bz2) then
-    echo "downloading hdf5-1.8.9.tar.gz"
-    wget http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.9.tar.bz2
-fi
-mkdir -p hdf5/parallel/src
-tar -jxf  hdf5-1.8.9.tar.bz2 -C hdf5/parallel/src
+#if !(test -e hdf5-1.8.9.tar.bz2) then
+#    echo "downloading hdf5-1.8.9.tar.gz"
+#    wget http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.9.tar.bz2
+#fi
+#mkdir -p hdf5/parallel/src
+#tar -jxf  hdf5-1.8.9.tar.bz2 -C hdf5/parallel/src
 
-mkdir -p hdf5/serial/src
-tar -jxf  hdf5-1.8.9.tar.bz2 -C hdf5/serial/src
+#mkdir -p hdf5/serial/src
+#tar -jxf  hdf5-1.8.9.tar.bz2 -C hdf5/serial/src
 
 
 #get netcdf sources
