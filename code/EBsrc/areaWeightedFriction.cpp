05e8fc7d244652a22a945307df4608048d8aaa0b
schwartz <schwartz@4b1697f3-9003-4f1e-b6a8-e72787e6a227>
2012-11-03 01:39:18 +0000

    progress...
    git-svn-id: https://anag-repo.lbl.gov/svn/BISICLES/public/trunk@1749 4b1697f3-9003-4f1e-b6a8-e72787e6a227

--- code/EBsrc/areaWeightedFriction.cpp
+++ code/EBsrc/areaWeightedFriction.cpp
@@ -8,112 +8,58 @@
 */
 #endif
 
-#include "twistyStreamFriction.H"
+#include "areaWeightedFriction.H"
 #include "IceConstants.H"
 #include "CONSTANTS.H"
 #include "BoxIterator.H"
 
 #include "NamespaceHeader.H"
 
-// -----------------------------------------------------------------
-//   twistyStreamFriction
-// -----------------------------------------------------------------
 
-
-/// constructor
-twistyStreamFriction::twistyStreamFriction()
+areaWeightedFriction::areaWeightedFriction() 
+: m_isValSet(false)
 {
 }
 
-twistyStreamFriction::twistyStreamFriction(const Real& a_betaVal,
-                                           const RealVect& a_omega,
-                                           const Real& a_magOffset,
-                                           const Real& a_eps,
-                                           const RealVect& a_domainSize)
-  : m_betaVal(a_betaVal), m_omega(a_omega), m_magOffset(a_magOffset), m_eps(a_eps), m_domainSize(a_domainSize)
+areaWeightedFriction::areaWeightedFriction(Real a_value)
+: m_frictionVal(a_value), m_isValSet(true)
 {
 
 }
- 
 
-/// destructor
-twistyStreamFriction::~twistyStreamFriction() 
+BasalFriction* areaWeightedFriction::new_basalFriction() const
 {
-}
-
-/// factory method
-/** return a pointer to a new BasalFriction object
- */
-BasalFriction* 
-twistyStreamFriction::new_basalFriction() const
-{
-  twistyStreamFriction* newPtr = new twistyStreamFriction;
-  newPtr->m_betaVal = m_betaVal;
-  newPtr->m_omega = m_omega;
-  newPtr->m_magOffset = m_magOffset;
-  newPtr->m_eps = m_eps;
-  newPtr->m_domainSize = m_domainSize;
+  areaWeightedFriction* newPtr = new areaWeightedFriction;
+  
+  newPtr->m_frictionVal = m_frictionVal;
+  newPtr->m_isValSet    = m_isValSet;
+  
   return static_cast<BasalFriction*>(newPtr);
-
 }
 
-/// define basal friction coefficient beta^2 and place in a_betaSqr
-/** time and dt are included in case this is time-dependent. Units 
-    should be Pa*a/m (any conversion to mks units is internal to the
-    AmrIce code)       
-*/
-void
-twistyStreamFriction::setBasalFriction(LevelData<FArrayBox>& a_betaSqr,
-                                       LevelSigmaCS& a_coordSys,
-                                       Real a_time,
-                                       Real a_dt)
+  /// define source term for thickness evolution and place it in flux
+  /** dt is included in case one needs integrals or averages over a
+      timestep
+  */
+void areaWeightedFriction::setBasalFriction(LevelData<FArrayBox>& a_betaSqr,
+                                        LevelSigmaCS        & a_coordSys,
+                                        Real                  a_time,
+                                        Real                  a_dt)
 {
-  // not quite correct in 3d
-  CH_assert(SpaceDim != 3);
-
-  RealVect twoPiOmega = m_omega;
-  twoPiOmega /= m_domainSize;
-  twoPiOmega *= 2.0*Pi;
-
-  RealVect dx = a_coordSys.dx();  
+  CH_assert(m_isValSet);
   DataIterator dit = a_betaSqr.dataIterator();
   for (dit.begin(); dit.ok(); ++dit)
     {
-      FArrayBox& thisBeta = a_betaSqr[dit];
-      
-      BoxIterator bit(thisBeta.box());
-      for (bit.begin(); bit.ok(); ++bit)
-        {
-          IntVect iv = bit();
-          RealVect x(iv);
-          x += 0.5*RealVect::Unit;
-          x *= dx;
-          
-          Real offset = m_magOffset*sin(twoPiOmega[0]*x[0]);
-          Real betaVal = m_betaVal*(1.0 +m_eps +D_TERM(1.0,
-                                                       *sin(twoPiOmega[1]*x[1]+offset),
-                                                       *sin(twoPiOmega[2]*x[2])));
-
-          thisBeta(iv,0) =betaVal;
-        }                         
+      a_betaSqr[dit].setVal(m_frictionVal);
     }
 }
 
-void 
-twistyStreamFriction::setParameters(const Real& a_betaVal, 
-                                    const RealVect& a_omega,
-                                    const Real& a_magOffset,
-                                    const Real& a_eps,
-                                    const RealVect& a_domainSize)
+///
+void areaWeightedFriction::setFrictionVal(const Real& a_betaSqr) 
 {
-  m_betaVal = a_betaVal;
-  m_omega = a_omega;
-  m_magOffset= a_magOffset;
-  m_eps = a_eps;
-  m_domainSize = a_domainSize;
+  m_frictionVal = a_betaSqr; 
+  m_isValSet    = true;
 }
-   
-
 
 
 #include "NamespaceFooter.H"
