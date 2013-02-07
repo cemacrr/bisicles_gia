97314fddb437499df8430ab2bde193b221868b84
schwartz <schwartz@4b1697f3-9003-4f1e-b6a8-e72787e6a227>
2013-02-07 21:46:32 +0000

    Added #if CH_USE_EB to distinguish 1D from 2D
    git-svn-id: https://anag-repo.lbl.gov/svn/BISICLES/public/trunk@1827 4b1697f3-9003-4f1e-b6a8-e72787e6a227

--- code/EBsrc/areaWeightedFriction.cpp
+++ code/EBsrc/areaWeightedFriction.cpp
@@ -9,57 +9,107 @@
 #endif
 
 #include "areaWeightedFriction.H"
+
 #include "IceConstants.H"
 #include "CONSTANTS.H"
 #include "BoxIterator.H"
+
+
+#ifdef CH_USE_EB
+#include "EBISBox.H"
+#include "EBIndexSpace.H"
+#include "VoFIterator.H"
+#endif
 
 #include "NamespaceHeader.H"
 
 
 areaWeightedFriction::areaWeightedFriction() 
-: m_isValSet(false)
+  :m_frictionPtr(NULL)
 {
 }
 
-areaWeightedFriction::areaWeightedFriction(Real a_value)
-: m_frictionVal(a_value), m_isValSet(true)
+areaWeightedFriction::areaWeightedFriction(BasalFriction* a_frictionPtr)
+  : m_frictionPtr(a_frictionPtr)
 {
-
 }
 
 BasalFriction* areaWeightedFriction::new_basalFriction() const
 {
   areaWeightedFriction* newPtr = new areaWeightedFriction;
   
-  newPtr->m_frictionVal = m_frictionVal;
-  newPtr->m_isValSet    = m_isValSet;
-  
+  newPtr->m_frictionPtr = m_frictionPtr;
+   
   return static_cast<BasalFriction*>(newPtr);
 }
 
-  /// define source term for thickness evolution and place it in flux
-  /** dt is included in case one needs integrals or averages over a
-      timestep
+  /// set the friction coefficient as grounded-area-Fraction*grounded value.
+  /** set the friction coefficient as grounded-area-Fraction*grounded value.
   */
 void areaWeightedFriction::setBasalFriction(LevelData<FArrayBox>& a_betaSqr,
-                                        LevelSigmaCS        & a_coordSys,
-                                        Real                  a_time,
-                                        Real                  a_dt)
+                                            LevelSigmaCS        & a_coordSys,
+                                            Real                  a_time,
+                                            Real                  a_dt)
 {
-  CH_assert(m_isValSet);
+  CH_assert(m_frictionPtr != NULL);
+  m_frictionPtr->setBasalFriction(a_betaSqr,a_coordSys,a_time,a_dt);
+
+  #ifdef CH_USE_EB
+
+  // ebisPtr
+  EBIndexSpace* ebisPtr = Chombo_EBIS::instance();
+
+  // if there are multiple levels, we need the problemDomain for the finest level
+  int validForSingleLevelCalculationOnly = 0;
+  ProblemDomain domain = ebisPtr->getBox(validForSingleLevelCalculationOnly);
+ 
+  // two extra ghost cells because Dan G. recommends extra ghost cells in the ebisl
+  EBISLayout ebisl;
+  int nGhost = 2;
+
+  // dbl
+  DisjointBoxLayout dbl = a_betaSqr.disjointBoxLayout();
+
+  // fill ebisl
+  ebisPtr->fillEBISLayout(ebisl, dbl, domain, nGhost);
+
+  //area weight the friction value
   DataIterator dit = a_betaSqr.dataIterator();
   for (dit.begin(); dit.ok(); ++dit)
     {
-      a_betaSqr[dit].setVal(m_frictionVal);
+      // geometry data and (trivial) connectivity for this box
+      EBISBox ebisBox = ebisl[dit()];
+
+      
+      // modify the value in the irreg ivs
+      Box box = dbl[dit()];
+          
+      // iterate over all irregular volumes of fluid
+      for (BoxIterator bit(box); bit.ok(); ++bit)
+        {
+          IntVect iv        = bit();
+          Vector<VolIndex> vofs = ebisBox.getVoFs(iv);
+          if (vofs.size() == 0)
+            {
+              a_betaSqr[dit()](iv,0) = 0.0;
+            }
+            else if (vofs.size() == 1)
+            {
+              Real areaFraction = ebisBox.volFrac(vofs[0]);
+              a_betaSqr[dit()](iv,0) *= areaFraction;
+            }
+            //else
+            {
+              MayDay::Error("Not coded for multi-cells");
+            }
+        }
+
     }
+#else
+  {
+  }
+#endif
+    
 }
-
-///
-void areaWeightedFriction::setFrictionVal(const Real& a_betaSqr) 
-{
-  m_frictionVal = a_betaSqr; 
-  m_isValSet    = true;
-}
-
 
 #include "NamespaceFooter.H"
