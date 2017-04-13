99b688f3930a7ecb24f45d52f706a1d30ba669be
dmartin <dmartin@4b1697f3-9003-4f1e-b6a8-e72787e6a227>
2017-04-13 18:44:03 +0000

    cleaning up long-ignored interface directory...
    git-svn-id: https://anag-repo.lbl.gov/svn/BISICLES/public/trunk@3365 4b1697f3-9003-4f1e-b6a8-e72787e6a227

--- code/interface/inputs/MISMIP3D/make_inputs.sh
+++
@@ -1,6 +0,0 @@
-for lev in 0 1 2 3 4 5; 
-do
-    tagcap=$(( lev - 1 )) 
-    sed -e s/@MAXLEVEL/$lev/ -e s/@TAGCAP/$tagcap/ inputs.mismip3D.stnd.template > inputs.mismip3D.stnd.l$lev
-    sed -e s/@MAXLEVEL/$lev/ mismip3D.stnd.template.config > mismip3D.stnd.l$lev.config
-done
--- code/interface/inputs/copyToBin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/csh -f
-touch Antarctica/.tempDummy
-foreach i (Antarctica/Antarctica_5km.config Antarctica/Antarctica_5km_withshelves.config Antarctica/Antarctica-noShelf.config Greenland/gis_1km.config  Greenland/gis_5km.config Greenland/gis-1km.zeroH.txt)
-if (-e $i) cp -f $i  ../../../../gc1/parallel/bin/;
-end;
-rm -f ../../lib/src/BoxTools/.tempDummy
-
-
