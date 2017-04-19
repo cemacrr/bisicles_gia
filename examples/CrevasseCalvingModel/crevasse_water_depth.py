98c97e647980ef3107340fd285fc52813ad563d1
slcornford <slcornford@4b1697f3-9003-4f1e-b6a8-e72787e6a227>
2017-04-19 08:46:39 +0000

    tidying up some examples
    git-svn-id: https://anag-repo.lbl.gov/svn/BISICLES/public/trunk@3375 4b1697f3-9003-4f1e-b6a8-e72787e6a227

--- examples/CrevasseCalvingModel/crevasse_water_depth.py
+++ examples/CrevasseCalvingModel/crevasse_water_depth.py
@@ -1,12 +1,15 @@
 
 def crevasse_water_depth(x,y,t,thck,topg,*etc):
+
+    
     
     depth = 0.0
-    if (t >= 50.0):
-        depth = 25.0
-    if (t >= 100.0):
-        depth = 20.0
-    if (t >= 550.0):
-        depth = 0.0
 
+    if (t >= 10.0):
+        depth = 20.0
+        
+    if (t >= 20.0):
+        depth = 0.0
+        
+    
     return depth 
